1b53845487fa117431b2bca5650e12cb9735fbe4
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-12-08 23:21:32 +0900

    patch up doc links
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -24,34 +24,40 @@
 //! Methods prefixed with `read_` *do* `consume` bytes. This means when this method is called, the
 //! cursor moves forward, which means the same bytes *cannot* be read multiple times.
 //!
+//! - [`BufReadExt::fill_be`] reads bytes as big-endian from a reader, doesn't consume bytes.
 //! - [`BufReadExt::fill_exact`] reads bytes until a buffer has been filled, doesn't consume bytes.
+//! - [`BufReadExt::fill_le`] reads bytes as little-endian from a reader, doesn't consume bytes.
+//! - [`BufReadExt::fill_ne`] reads bytes using native endianness from a reader, doesn't consume bytes.
 //! - [`BufReadExt::fill_until`] reads bytes until a byte has been encountered, doesn't consume bytes.
 //! - [`BufReadExt::fill_while`] reads bytes based on a predicate, doesn't consume bytes.
 //! - [`BufReadExt::read_while`] reads bytes based on a predicate, consumes bytes.
+//! - [`BufReadExt::skip`] Skip the first `n` bytes.
+//! - [`BufReadExt::skip_until`] Skip bytes until the delimiter `byte` or EOF is reached.
+//! - [`BufReadExt::skip_while`] Skip bytes while a predicate is true.
 //! - [`ReadExt::read_be`] reads bytes as big-endian from a reader, consumes bytes.
 //! - [`ReadExt::read_le`] reads bytes as little-endian from a reader, consumes bytes.
 //! - [`ReadExt::read_ne`] reads bytes using native endianness from a reader, consumes bytes.
-//! - [`BufReadExt::fill_be`] reads bytes as big-endian from a reader, doesn't consume bytes.
-//! - [`BufReadExt::fill_le`] reads bytes as little-endian from a reader, doesn't consume bytes.
-//! - [`BufReadExt::fill_ne`] reads bytes using native endianness from a reader, doesn't consume bytes.
 //! - [`WriteExt::write_be`] write bytes as big-endian to a writer.
 //! - [`WriteExt::write_le`] write bytes as little-endian to a writer.
 //! - [`WriteExt::write_ne`] write bytes using native endianness to a writer.
 //!
-//! [`consume`]: https://doc.rust-lang.org/std/io/trait.BufRead.html#tymethod.consume
+//! [`BufReadExt::fill_be`]: trait.BufReadExt.html#method.fill_be
 //! [`BufReadExt::fill_exact`]: trait.BufReadExt.html#method.fill_exact
+//! [`BufReadExt::fill_le`]: trait.BufReadExt.html#method.fill_le
+//! [`BufReadExt::fill_ne`]: trait.BufReadExt.html#method.fill_ne
 //! [`BufReadExt::fill_until`]: trait.BufReadExt.html#method.fill_until
 //! [`BufReadExt::fill_while`]: trait.BufReadExt.html#method.fill_while
 //! [`BufReadExt::read_while`]: trait.BufReadExt.html#method.read_while
+//! [`BufReadExt::skip`]: trait.BufReadExt.html#method.skip
+//! [`BufReadExt::skip_until`]: trait.BufReadExt.html#method.skip_until
+//! [`BufReadExt::skip_while`]: trait.BufReadExt.html#method.skip_while
 //! [`ReadExt::read_be`]: trait.ReadExt.html#method.read_be
 //! [`ReadExt::read_le`]: trait.ReadExt.html#method.read_le
 //! [`ReadExt::read_ne`]: trait.ReadExt.html#method.read_ne
-//! [`BufReadExt::fill_be`]: trait.BufReadExt.html#method.fill_be
-//! [`BufReadExt::fill_le`]: trait.BufReadExt.html#method.fill_le
-//! [`BufReadExt::fill_ne`]: trait.BufReadExt.html#method.fill_ne
 //! [`WriteExt::write_be`]: trait.WriteExt.html#method.write_be
 //! [`WriteExt::write_le`]: trait.WriteExt.html#method.write_le
 //! [`WriteExt::write_ne`]: trait.WriteExt.html#method.write_ne
+//! [`consume`]: https://doc.rust-lang.org/std/io/trait.BufRead.html#tymethod.consume
 //!
 //! # Todos
 //!
